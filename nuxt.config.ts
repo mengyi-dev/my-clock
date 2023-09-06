97424b33621699abc37592da66324c8b35f5adcc
Mengyi YOENG <90229183+mengyi-dev@users.noreply.github.com>
2023-09-06 17:05:15 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,7 +25,7 @@ export default defineNuxtConfig({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WHDS4FC');`,
+            })(window,document,'script','dataLayer','GTM-TSTT29SH');`,
         },
       ],
     },
